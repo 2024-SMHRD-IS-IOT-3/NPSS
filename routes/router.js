b52d88c94fd2c0d75fa9b662be83e7f456c5d063
CSB <soob0513@gmail.com>
2024-05-13 14:47:21 +0900

    update react-project

---
+++ routes/router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const conn = require('../config/db');
+const path = require('path');
+
+// 메인 경로
+router.post('/', (req, res)=>{
+    console.log("누군가 메인페이지에 접근!")
+    res.sendFile(path.join(__dirname, "..", "react-project", "build", "index.html"))
+})
+
+
+module.exports = router;
