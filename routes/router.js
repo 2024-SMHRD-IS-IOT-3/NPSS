56b3085903406be5008b383c5c5745e7c738e0d2
hj <201140@jnu.ac.kr>
2024-05-13 16:50:34 +0900

    DB연결

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const router = express.Router();
-const conn = require('../config/db');
+const connectToOracle  = require('../config/db'); // 데이터베이스 연결 객체를 가져옴
 const path = require('path');
 
 // 메인 경로
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     console.log("누군가 메인페이지에 접근!")
     res.sendFile(path.join(__dirname, "..", "react-project", "build", "index.html"))
+})
+
+// 회원 가입을 담당하는 경로
+router.post('/handleJoin', async (req, res) => { // 비동기 함수로 변경
+    console.log('누군가 회원 가입을 희망합니다.', req.body);
+
+    let { id, pw, email } = req.body;
+
+    try {
+        const connection = await connectToOracle(); // 데이터베이스 연결 객체를 가져옴
+
+        // // 1. 회원가입을 할 수 있는 쿼리문 작성 - sql
+        // let sql = `INSERT INTO USER_INFO (USER_ID, USER_PW, USER_EMAIL, JOINED_AT) VALUES (:id, :pw, :email, SYSDATE)`;
+
+        // // 2. DB에 연결해서 쿼리문을 실행
+        // const result = await connection.execute(sql, {
+        //     id: id,
+        //     pw: pw,
+        //     email: email,
+
+        // });        
+        
+        // 1. 회원가입을 할 수 있는 쿼리문 작성 - sql
+        let sql = `INSERT INTO USER_INFO VALUES(:1, :2, :3, SYSDATE)`;
+
+        // 2. DB에 연결해서 쿼리문을 실행
+        result = await connection.execute(sql,[id,pw,email]);
+        console.log(result);
+        
+        // 쿼리 실행 결과에 따라 응답을 보냄
+        if (result.rowsAffected && result.rowsAffected > 0) {
+            res.send({ result: "success" });
+        } else {
+            res.send({ result: "fail" });
+        }
+    } catch (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send({ result: 'fail' });
+    }
 })
 
 
